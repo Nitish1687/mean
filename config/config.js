3415fae2f2bc781567c34bfe61a83decccb6c5fa
Shukla <nitish.shukla@crest.in>
2017-02-05 16:00:44 +0530

    property files as per env

---
+++ config/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by nsm1211 on 2/5/17.
+ */
+
+module.exports = require('./env'
+    + process.env.NODE_ENV
+    + '.js'
+);
---
+++ config/env/development.js
@@ -0,0 +1,5 @@
+/**
+ * Created by nsm1211 on 2/5/17.
+ */
+
+module.exports = {};
