256516896a6b0d40c97dfd0170fd3eef691b0510
Shukla <nitish.shukla@crest.in>
2017-02-05 15:19:43 +0530

    middleware morgan(logger),body-parser and env setting

--- config/express.js
+++ config/express.js
@@ -3,14 +3,39 @@
  */
 
 const express = require('express');
+const morgan = require('morgan');
+const compress = require('compression');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
 
 module.exports = () => {
 
     const app = express();
+
+    enviromentSetting(app);
+    middleWareSetting(app);
 
     const indexRoutes = require('../app/routes/index.server.routes');
 
     indexRoutes(app);
 
     return app;
+};
+
+enviromentSetting = function (app) {
+    if (process.env.NODE_ENV == 'development') {
+        app.use(morgan('dev'));
+    } else if (process.env.NODE_ENV == 'production') {
+        app.use(compress());
+    }
+};
+
+middleWareSetting = function (app) {
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(bodyParser.json());
+    app.use(methodOverride());
 };
