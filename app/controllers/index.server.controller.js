5b32abf4d3ea2bf82d3ae380000c5dc227baa1e7
Shukla <nitish.shukla@crest.in>
2017-02-05 22:33:43 +0530

    employee list mustache template

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -5,5 +5,23 @@ exports.render = (req, res) => {
     }
 
     req.session.lastVisit = new Date();
-    res.render('index', {title: 'Welcome Page'});
+    res.render('index',
+        {
+            title: 'Welcome Page',
+            description: "Employee List",
+            employees: [
+                {
+                    name: 'nitish',
+                    age: '29'
+                },
+                {
+                    name: 'rahul',
+                    age: '25'
+                },
+                {
+                    name: 'prashant',
+                    age: '28'
+                }
+            ]
+        });
 };
